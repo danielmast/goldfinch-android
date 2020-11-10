0d813b0a67f0708681e1a3a47c44c3871d7693e1
danielmast <danielmast92@gmail.com>
2020-11-10 19:43:31 +0100

    Init Android project

---
+++ app/src/main/java/nl/danielmast/goldfinch/goldfinchandroid/MainActivity.kt
@@ -0,0 +1,11 @@
+package nl.danielmast.goldfinch.goldfinchandroid
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
